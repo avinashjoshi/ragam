9d1c2ead0ad007985a44cc529e0e98828c63e547
Avinash Joshi <avinash.joshi@utdallas.edu>
2012-10-01 19:16:55 -0500

    Removed all CLI. Simple ./ragam is THE way

--- helper.c
+++ helper.c
@@ -9,25 +9,6 @@
 
 #include "my_header.h"
 
-const char *com_list[] = {"help", "makemaster", "connect", "NULL"};
-
-/*
- * is_command takes string as argument and checks
- * if that is present in com_list[] array
- * 
- * Return 0 if not present else returns 1
- */
-int
-is_command ( char *com ) {
-	int i = 0;
-	while (strcmp ("NULL", com_list[i]) != 0) {
-		if (strcmp (com, com_list[i]) == 0)
-			return 1;
-		i++;
-	}
-	return 0;
-}
-
 /*
  * get_program_name takes string as input
  * and gets the file (executable) name
--- main.c
+++ main.c
@@ -2,61 +2,20 @@
 
 const char *prog_name;
 
-void
-print_help ( void ) {
-	fprintf ( stdout, "%s: takes 1 argument - makemaster, connect\n", prog_name );
-	return;
-}
-
 int
 main ( int argc, char *argv[] ) {
 
 	/* Variable Declerations */
 	prog_name = get_program_name ( argv[0] );
 
-	/* Program must have atleast one argument */
+#ifndef LISTEN_PORT
+	/* Listen port not defined
+	 * => Program must accept port */
 	if ( argc < 2 ) {
-		fprintf ( stderr, "error: %s: must have atleast one argument\n", prog_name );
-		print_help ();
+		fprintf ( stderr, "error: %s: Port number needed to continue...\n", prog_name );
 		exit ( EXIT_FAILURE );
 	}
-
-	/* Is the second argument a valid command? */
-	if ( !is_command ( argv[1] )) {
-		fprintf ( stderr, "error: %s: %s is an invalid command\n", prog_name, argv[1] );
-		exit ( EXIT_FAILURE );
-	}
-
-	/* Creating a master? */
-	if ( strcmp ( argv[1], "makemaster" ) == 0 ) {
-#ifndef LISTEN_PORT
-		if ( argc != 3 ) {
-			fprintf ( stderr, "error: %s: makemaster command needs port_number\n", prog_name );
-			exit ( EXIT_FAILURE );
-		}
 #endif
-		// Do Make server
-	}
-
-	/* Connecting to a master? */
-	if ( strcmp ( argv[1], "connect" ) == 0 ) {
-		if ( argc < 3 ) {
-#ifndef LISTEN_PORT
-			fprintf ( stderr, "error: %s: connect command needs \"IP port_number\"\n", prog_name );
-#else
-			fprintf ( stderr, "error: %s: connect command needs IP\n", prog_name );
-#endif
-			exit ( EXIT_FAILURE );
-		}
-
-#ifndef LISTEN_PORT
-		if ( argc != 4 ) {
-			fprintf ( stderr, "error: %s: connect command needs \"IP port_number\"\n", prog_name );
-			exit ( EXIT_FAILURE );
-		}
-#endif
-		// Connect to master
-	}
 
 	return EXIT_SUCCESS;
 }
